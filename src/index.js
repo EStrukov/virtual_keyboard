8cd7279f561acab0d32c2490d64866f8dc38847d
Eugeny <88819174+EStrukov@users.noreply.github.com>
2022-05-08 20:52:39 +0300

    feat: capslock is work maybe

--- src/index.js
+++ src/index.js
@@ -13,18 +13,19 @@ const keyboardStringCode1 = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4'
 document.body.innerHTML = '<textarea class="inputina"></textarea><div class="wrapper"></div>';
 let wrapper = document.querySelector('.wrapper');
 
-// function createUpperKeyboard() {
-//   let numString = '';
-//   let div1 = document.createElement('div');
-//   div1.className = 'keystring';
-//   for (let i = 0; i < keyboardString1.length; i++) {
-//     numString += '<div class="key" data-key="' + keyboardStringCode1[i] + '">' + keyboardString1[i].toUpperCase() + '</div>';
-//   }
-//   div1.innerHTML = numString;
-//   wrapper.append(div1);
-//   numString = '';
-// }
-
+function getCaretPos(obj) {
+  obj.focus();
+  if (obj.selectionStart) return obj.selectionStart;
+  else if (document.selection) {
+    var sel = document.selection.createRange();
+    var clone = sel.duplicate();
+    sel.collapse(true);
+    clone.moveToElementText(obj);
+    clone.setEndPoint('EndToEnd', sel);
+    return clone.text.length;
+  }
+  return 0;
+}
 
 function createKeyboard() {
   let numString = '';
@@ -36,12 +37,30 @@ function createKeyboard() {
   div1.innerHTML = numString;
   wrapper.append(div1);
   numString = '';
-
 }
 createKeyboard();
 
+let capsLock = document.querySelector('.key[data-key="CapsLock"]');
+let checkBox = document.createElement('input');
+checkBox.setAttribute('type', 'checkbox');
+capsLock.append(checkBox);
 let text = document.querySelector('textarea');
 let keys = document.querySelectorAll('.key');
+
+
+function setSelectionRange(input, selectionStart, selectionEnd) {
+  if (input.setSelectionRange) {
+    input.focus();
+    input.setSelectionRange(selectionStart, selectionEnd);
+  } else if (input.createTextRange) {
+    var range = input.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', selectionEnd);
+    range.moveStart('character', selectionStart);
+    range.select();
+  }
+}
+
 function sizeBtn() {
   document.querySelector('.key[data-key="Space"]').style.width = 300 + 'px';
   document.querySelector('.key[data-key="Enter"]').style.width = 100 + 'px';
@@ -56,24 +75,11 @@ function sizeBtn() {
 }
 sizeBtn();
 
-// function upperOrlower() {
-//   if (event.shiftKey === true) {
-//     wrapper.innerHTML = '';
-//     createUpperKeyboard();
-//     sizeBtn();
-//   } else {
-//     wrapper.innerHTML = '';
-//     createKeyboard();
-//     sizeBtn();
-// }
-// }
-
-
-document.addEventListener('keydown', (event) => {  
+document.addEventListener('keydown', (event) => {
   keys.forEach(e => {
     if (event.code === e.dataset.key) {
       e.classList.add('active');
-    } 
+    }
   });
 });
 
@@ -81,7 +87,7 @@ document.addEventListener('keyup', (event) => {
   keys.forEach(e => {
     if (event.code === e.dataset.key) {
       e.classList.remove('active');
-    } 
+    }
   });
 });
 
@@ -90,8 +96,30 @@ keys.forEach(e => {
     text.focus();
     event.preventDefault();
     e.classList.add('active');
-    console.log(event);
-    text.value = text.value + event.target.innerHTML;
+
+    if (event.target.dataset.key != 'CapsLock' && event.target.dataset.key != 'ControlLeft' && event.target.dataset.key != 'ControlRight' && event.target.dataset.key != 'AltLeft' &&
+      event.target.dataset.key != 'AltRight' && event.target.dataset.key != 'Enter' && event.target.dataset.key != 'Backspace' && event.target.dataset.key != 'Delete' &&
+      event.target.dataset.key != 'ShiftRight' && event.target.dataset.key != 'ShiftLeft' && event.target.dataset.key != 'Tab' && event.target.dataset.key != 'MetaLeft') {
+      if (checkBox.checked == false) {
+        text.value = text.value + event.target.innerHTML;
+      } else {
+        text.value = text.value + event.target.innerHTML.toUpperCase();
+      }
+    } else if (event.target.dataset.key === 'Backspace') {
+      let back = getCaretPos(text);
+      let tmp = text.value.split('');
+      tmp.splice(back - 1, 1);
+      text.value = tmp.join('');
+      setSelectionRange(text, back, back);
+    } else if (event.target.dataset.key === 'Delete') {
+      let back = getCaretPos(text);
+      let tmp = text.value.split('');
+      tmp.splice(back, 1);
+      text.value = tmp.join('');
+      setSelectionRange(text, back, back);
+    } else if (event.target.dataset.key === 'Tab') {
+      text.value = text.value + '    ';
+    }
   });
 });
 
@@ -99,8 +127,32 @@ keys.forEach(e => {
   text.focus();
   e.addEventListener('mouseup', (event) => {
     event.preventDefault();
-    console.log(e);
     e.classList.remove('active');
   });
 });
 
+capsLock.addEventListener('click', () => {
+  if (checkBox.checked !== true) {
+    checkBox.checked = true;
+  } else {
+    checkBox.checked = false;
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  text.focus();
+  if (event.key == 'CapsLock') {
+    if (checkBox.checked !== true) {
+      checkBox.checked = true;
+    } else {
+      checkBox.checked = false;
+    }
+  }
+  if (event.key == 'Tab') {
+    event.preventDefault();
+    text.value = text.value + '    ';
+  }
+  if (event.key == 'MetaLeft') {
+    event.preventDefault();
+  }
+});
